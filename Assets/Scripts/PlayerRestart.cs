c14fcfea23363da3fef403057cc60e5a70d5c45e
EricRDelgado <erdelgado@my.waketech.edu>
2023-09-03 15:52:18 -0400

    FixedPlayerRestart

---
+++ Assets/Scripts/PlayerRestart.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerRestart : MonoBehaviour
+{
+    public static string LevelBefore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Level_1" || SceneManager.GetActiveScene().name == "Level_2" || SceneManager.GetActiveScene().name == "Level_3")
+        {
+            LevelBefore = SceneManager.GetActiveScene().name;
+        }
+        Debug.Log(LevelBefore);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClickRestart()
+    {
+        SceneManager.LoadScene(LevelBefore);
+    }
+}
