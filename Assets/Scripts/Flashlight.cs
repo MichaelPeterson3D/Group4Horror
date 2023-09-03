ac6006681277086854713cb42bddeacaae8e48cf
EricRDelgado <erdelgado@my.waketech.edu>
2023-09-03 17:11:44 -0400

    Added Colider Fixes

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -103,7 +103,7 @@ public class Flashlight : MonoBehaviour
             if (enemyLists[i].GetComponent<EnemyFlash>().IsCamInEnemyView == true)
             {
                 
-                StartCoroutine(enemyLists[i].GetComponent<EnemyMovement>().StopEnemyforAFewSec(3.0f));
+                StartCoroutine(enemyLists[i].GetComponent<EnemyMovement>().StopEnemyforAFewSec(4.0f));
             }
         }
     }
