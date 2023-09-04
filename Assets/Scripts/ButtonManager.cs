156cf96ad165c88952663c2f831a73711c38b0d1
krdduncan96 <krdduncan@gmail.com>
2023-09-04 18:04:21 -0400

    sound/text fixes, stairs

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -14,14 +14,14 @@ public class ButtonManager : MonoBehaviour
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
 
-            GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().StopMusic();
+            //GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().StopMusic();
         }
         else
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
 
-            GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().PlayMusic();
+            //GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().PlayMusic();
         }
         
     }
