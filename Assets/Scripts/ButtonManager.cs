0cf673bde7ebff4972cad2be606d92499084689b
krdduncan96 <krdduncan@gmail.com>
2023-09-09 00:53:20 -0400

    Revert "boop"
    This reverts commit eee9ef1b9bfb71cb2016d1bc8e80e6029996a74d.

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -13,6 +13,8 @@ public class ButtonManager : MonoBehaviour
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
+
+            GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().StopMusic();
         }
         else
         {
@@ -31,7 +33,6 @@ public class ButtonManager : MonoBehaviour
     }
     public void OnClickPlay()
     {
-        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicController>().StopMusic();
         SceneManager.LoadScene("Level_1");
     }
     public void OnClickMainMenu()
